8b157fdf44645c671519eda6a5da5dfc7fde9b90
Simo <128434490+simo665@users.noreply.github.com>
2024-11-06 04:22:50 +0100

    Update Text Reverse.py

--- python/Text Reverse.py
+++ python/Text Reverse.py
@@ -1,7 +1,4 @@
-text = input("Enter a text: ")
-text_list = text.split()
-reverse_txt = []
-for word in text_list[::-1]:
-	reverse_txt.append(word)
-print(text_list)
-print(reverse_txt)
+text = input("Enter a text: ").split()
+print("Reversed sentence: ", end="")
+reverse_sentences = " ".join(text[::-1])
+print(reverse_sentences)
