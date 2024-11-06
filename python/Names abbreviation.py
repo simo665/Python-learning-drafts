18c84e78ff326bb88d35986109294b764b04670b
Simo <128434490+simo665@users.noreply.github.com>
2024-11-06 04:11:37 +0100

    Update Names abbreviation.py

--- python/Names abbreviation.py
+++ python/Names abbreviation.py
@@ -1,15 +1,14 @@
-friends_list = input("Enter your friends first and last name sperated by a comma: ")
+friends_list = input("Enter your friends first and last name sperated by a comma: ").split(", ")
 
-friends_list = friends_list.split(", ")
 shortcuts_list = []
 
 for friend in friends_list:
     friend_name = friend.split()
     print(friend_name)
     
-    f1 = friend_name[0][0]
-    f2 = friend_name[1][0]
-    shortcut = f"{f1}.{f2}.".upper()
+    first_name = friend_name[0][0]
+    last_name = friend_name[1][0]
+    shortcut = f"{first_name}.{last_name}.".upper()
     shortcuts_list.append(shortcut)
     
 print("\nAbbreviated names:\n")
